241c14afa8a26cd2968ba9dd20838753ff5b19f8
J.Crhistian <j.crhistiantr@gmail.com>
2026-04-22 17:28:56 -0400

    Adaptar el Docker para que funcione de forma correcta con Mongo, ademas de probar su conexion

--- backend/src/routes/tasks.ts
+++ backend/src/routes/tasks.ts
@@ -1,7 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import env from "../Config/env";
 
 export const taskrouter = express.Router();
 
-taskrouter.get("/", (req, res) =>{
-    res.json("probando");
+taskrouter.get("/", async (req, res) =>{
+    try
+    {
+        await mongoose.connect(env.MONGO_URI);
+        res.json("funciono bien");
+    }
+    catch
+    {
+        res.status(500).json("Fallo")
+        console.log("Falle");
+    }
 })
